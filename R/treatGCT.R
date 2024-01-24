d0bfb370d32fafaf7aec44d8eae14c618fe941e3
Antonia <45034285+iovlaicu@users.noreply.github.com>
2024-01-24 13:37:57 +0100

    Update treatGCT.R

--- R/treatGCT.R
+++ R/treatGCT.R
@@ -1,10 +1,12 @@
 treatGCT <- function(lGCT, window=ceiling(as.numeric(WINDOW)/as.numeric("10000")))
 {
     nlGCT <- lapply(lGCT,function(gc)
-    {
-        l <- length(gc)
+    {    
+        print("beforenchar")
+        l <- nchar(gc)
         starts <- getstartends(end=l,window=window)$starts
         ends <- getstartends(end=l,window=window)$ends
+        print("afternchar")
         gc <- sapply(1:length(starts),function(x) mean(gc[starts[x]:ends[x]]))
     })
     names(nlGCT) <- names(lGCT)
