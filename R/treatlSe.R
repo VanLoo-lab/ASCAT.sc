cba63732b7ece5e98943718baef4a14aaef85d3a
Antonia <45034285+iovlaicu@users.noreply.github.com>
2024-02-01 04:20:33 +0100

    Update treatlSe.R

--- R/treatlSe.R
+++ R/treatlSe.R
@@ -1,12 +1,10 @@
 treatlSe <- function(lSe, window=ceiling(as.numeric(WINDOW)/as.numeric("10000")))
 {
     nlSe <- lapply(lSe,function(x)
-    {    
-        print("beforenchar2")
-        l <- nchar(x$starts)
+    {
+        l <- length(x$starts)
         starts <- getstartends(end=l,window=window)$starts
         ends <- getstartends(end=l,window=window)$ends
-        print("afternchar2")
         list(starts=x$starts[starts],
              ends=x$ends[ends])
     })
