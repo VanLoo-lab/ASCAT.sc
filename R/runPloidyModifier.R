77cda1f19f809016e0536c4e45fd93308ce35265
iovlaicu <Ioana-Antonia.Vlaicu@ulb.be>
2024-02-23 18:17:17 +0100

    update runploidyModifier.R

--- R/runPloidyModifier.R
+++ R/runPloidyModifier.R
@@ -2,5 +2,5 @@ run_PloidyModifier <- function(){
   suppressPackageStartupMessages(require(shiny))
    
   appdir <- paste0(system.file(package = "ASCAT.sc"), "/PloidyModifier/")
-  shiny::runApp(appdir)
+  shiny:::runApp(appdir)
   }
