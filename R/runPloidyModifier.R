049dab3c7fef0991a6192eec6a0ecfb499d2fcc0
iovlaicu <Ioana-Antonia.Vlaicu@ulb.be>
2024-02-26 16:57:12 +0100

    Changed location of PloidyModifier app

--- R/runPloidyModifier.R
+++ R/runPloidyModifier.R
@@ -1,6 +1,6 @@
 run_PloidyModifier <- function(){
   suppressPackageStartupMessages(require(shiny))
    
-  appdir <- paste0(system.file(package = "ASCAT.sc"), "/PloidyModifier/")
+  appdir <- paste0(system.file(package = "ASCAT.sc"), "/inst/PloidyModifier/")
   shiny:::runApp(appdir)
   }
