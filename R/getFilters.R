8a4bd17e0d7f132fea03816c55a3f71758f2ea61
galder-max <maximetarabichi@Maximes-MacBook-Pro.local>
2025-12-23 12:36:26 +0100

    improve sc_filters

--- R/getFilters.R
+++ R/getFilters.R
@@ -1,5 +1,6 @@
 getFilters <- function(res,
                        probs=.1,
+                       threshold_extra_noise=0.02, ##less than this away from the median noise across cells with same number of reads (sd on logr).
                        outdir="./",
                        projectname="")
 {
@@ -29,21 +30,35 @@ getFilters <- function(res,
         nrecords  <-  sapply(allT,function(x) sum(unlist(lapply(x$lCTS,function(y) y$records))))
         thresholdNrec <- quantile(nrecords,probs=probs)## removing 10% cells with lowest read counts
         ambiguous <- sapply(allS,function(x) x$ambiguous)
-        doublet <- sapply(allS,function(x) if(!is.null(x$bestfit)) !x$bestfit$ambiguous else F)
+        doublet <- sapply(allS,function(x) if(!is.null(x$bestfit)) !x$bestfit$ambiguous else F) ## not used
         qualities <- getQuality.SD(allT)
-        thresholdQual <- quantile(qualities,probs=1-probs)## removing 10% cells with lowest read counts
-        keep <- qualities<=thresholdQual & nrecords>=thresholdNrec & !ambiguous & !doublet
+        thresholdQual <- quantile(qualities,probs=1-probs)## removing 10% cells with highest noise
+        keep <- qualities<=thresholdQual & nrecords>=thresholdNrec & !ambiguous
         keep2 <- !(qualities<thresholdQual & nrecords<thresholdNrec)
         keep2 <- keep2 & !ambiguous
         ll <- getloess(qualities[keep2], log2(nrecords)[keep2])
         ord <- order(log2(nrecords)[keep2],decreasing=F)
-        filters <- (1:length(nrecords))%in%(which(keep2)[abs(ll$residuals)<=0.02]) & keep
+        filters <- (1:length(nrecords))%in%(which(keep2)[ll$residuals<=threshold_extra_noise]) & keep
+        df <- data.frame(sample_cell=names(allT),
+                         n_reads_total=nrecords,
+                         sd_noise=qualities,
+                         ambiguous_flag=ambiguous,
+                         noise_filter_out=qualities>=thresholdQual,
+                         n_reads_filter_out=nrecords<thresholdNrec,
+                         keep_cell_filter=filters)
+        ## ##################################################################
+        write.table(df,
+                    file=paste0(outdir,"/",projectname,"_filters.tsv"),
+                    sep="\t",
+                    quote=F,
+                    col.names=T,
+                    row.names=F)
         ## ##################################################################
         pdf(paste0(outdir,"/",projectname,"_sc_filters.pdf"))
         plot(qualities,
              log2(nrecords),
-             xlab="Noise logr",
-             ylab="Total number of reads",
+             xlab="Noise in logr (sd)",
+             ylab="Total number of reads (log2)",
              pch=ifelse(doublet,15,19),
              cex=ifelse(doublet,.3,.1),
              col=ifelse(ambiguous,rgb(1,0,.5,.5),rgb(0,0,0,.5)))
@@ -51,14 +66,12 @@ getFilters <- function(res,
         abline(v=thresholdQual,h=log2(thresholdNrec))
         points(qualities[keep2],
                log2(nrecords)[keep2],
-               col=ifelse(abs(ll$residuals)>0.02, rgb(1,0,0,1), rgb(0,0,0,0)),cex=.15)
-        try({plot(density(log2(nrecords[!doublet])))
-            polygon(density(log2(nrecords[doublet])),border="red")
-        },silent=T)
+               col=ifelse(ll$residuals>0.02, rgb(1,0,0,1), rgb(0,0,0,0)),cex=.15)
+        plot(density(log2(nrecords)), xlab="log2(number of reads)", ylab="Density", main="Distribution of #reads across cells")
         plot(qualities,
              log2(nrecords),
-             xlab="Noise logr",
-             ylab="Total number of reads",
+             xlab="Noise in logr (sd)",
+             ylab="Total number of reads (log2)",
              pch=ifelse(filters,15,19),
              cex=ifelse(filters,.3,.1),
              col=ifelse(filters,rgb(1,0,.5,.5),rgb(0,0,0,.5)))
@@ -66,5 +79,6 @@ getFilters <- function(res,
         ## ##################################################################
     })
     res$filters <- filters
+    res$filters_data_frame <- df
     res
 }
